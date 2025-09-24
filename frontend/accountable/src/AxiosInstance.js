82c173463d485be447bc939cf1487024caf89879
Sameer Khan <samsam23122001@gmail.com>
2025-09-24 03:31:17 -0400

    Revert "Update AxiosInstance.js"
    This reverts commit ed48f346b39363d6654bbcc657b36b0dbd1eae39.

--- frontend/accountable/src/AxiosInstance.js
+++ frontend/accountable/src/AxiosInstance.js
@@ -1,34 +1,34 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-import { useAuth0 } from "@auth0/auth0-react";
+import axios from 'axios';
+import { useAuth0 } from '@auth0/auth0-react';
+import { useEffect, useState } from 'react';
 
 export const useAxios = () => {
-  const { getAccessTokenSilently, isAuthenticated } = useAuth0();
-  const [axiosInstance, setAxiosInstance] = useState(null);
+    const { getAccessTokenSilently, isAuthenticated } = useAuth0();
 
-  useEffect(() => {
-    if (isAuthenticated) {
-      getAccessTokenSilently()
-        .then((token) => {
-          const instance = axios.create({
-            baseURL: process.env.REACT_APP_API_URL,
-            headers: {
-              "Content-Type": "application/json",
-              Accept: "application/json",
-            },
-          });
+    //const[token, setToken] = useState('');
+    const [axiosInstance, setAxiosInstance] = useState(null);
 
-          // ✅ attach token correctly
-          instance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 
-          setAxiosInstance(instance);
-        })
-        .catch((error) => {
-          console.error("Error getting token:", error);
-          setAxiosInstance(null);
-        });
-    }
-  }, [isAuthenticated, getAccessTokenSilently]);
+    useEffect(()=>{
+        if (isAuthenticated) {
+            getAccessTokenSilently()
+            .then(token =>  {
+                const instance = axios.create({
+                    baseURL: process.env.REACT_APP_API_URL,
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Accept: 'application/json'
+                    }
+                });
+                instance.defaults.headers.Authorization = `Bearer ${token}`;
+                setAxiosInstance({instance});
+            })
+            .catch(error => {
+                console.log(error);
+                setAxiosInstance(null);
+            })
+        }
+    }, [isAuthenticated, getAccessTokenSilently])
 
-  return axiosInstance;
+    return axiosInstance? axiosInstance.instance : null;
 };
